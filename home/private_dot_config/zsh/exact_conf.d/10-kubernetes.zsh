3febf88bad3694806481208b6906b07ad0dfab3b
Björn Kahlert <bjoern.kahlert@ista.com>
2026-03-08 14:05:16 +0100

    refactor: move kubernetes PATH entries to 10-kubernetes.zsh

--- home/private_dot_config/zsh/exact_conf.d/10-kubernetes.zsh
+++ home/private_dot_config/zsh/exact_conf.d/10-kubernetes.zsh
@@ -1,1 +1,2 @@
+PATH="$HOME/.krew/bin:$HOME/.arkade/bin:$PATH"
 export KUBE_EDITOR="ideaw --wait"
--- home/private_dot_config/zsh/exact_conf.d/10-path.zsh
+++ home/private_dot_config/zsh/exact_conf.d/10-path.zsh
@@ -1,8 +1,5 @@
 # Local bins
 PATH="$HOME/.local/bin:/usr/local/sbin:$PATH"
 
-# Kubernetes tools
-PATH="$HOME/.krew/bin:$HOME/.arkade/bin:$PATH"
-
 # Deduplicate PATH
 export -U PATH
